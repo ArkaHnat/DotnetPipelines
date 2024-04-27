ef4a359b7967c4e7895db320e8941f30dbe8b4ec
ahaw <ahaw@o2.pl>
2024-04-27 08:44:37 +0200

    DependabotCommitModule should not throw exception when release not found

--- src/ModularPipelines.Build/Modules/DependabotCommitsModule.cs
+++ src/ModularPipelines.Build/Modules/DependabotCommitsModule.cs
@@ -9,31 +9,38 @@ using Octokit;
 namespace ModularPipelines.Build.Modules;
 
 [SkipIfNoGitHubToken]
-[RunOnLinuxOnly]
 public class DependabotCommitsModule : Module<List<string>>
 {
     protected override async Task<List<string>?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
     {
         var repositoryInfo = context.GitHub().RepositoryInfo;
-        
-        var latestRelease = await context.GitHub().Client.Repository.Release.GetLatest(repositoryInfo.Owner, repositoryInfo.RepositoryName);
+        try
+        {
+            var latestRelease = await context.GitHub().Client.Repository.Release.GetLatest(repositoryInfo.Owner, repositoryInfo.RepositoryName);
 
-        var commitsSinceRelease = await context.GitHub().Client.Repository.Commit.GetAll(repositoryInfo.Owner,
-            repositoryInfo.RepositoryName, new CommitRequest
-            {
-                Sha = "main",
-                Since = latestRelease.CreatedAt.AddMinutes(-2),
-            });
-        
-        var commits = commitsSinceRelease
-            .Where(x => x.Author.Login.StartsWith("dependabot"))
-            .Select(x => x.Commit.Message.Split(Environment.NewLine))
-            .Select(x => x.FirstOrDefault())
-            .OfType<string>()
-            .ToList();
-        
-        context.Logger.LogInformation("Commits: {Commits}", string.Join(Environment.NewLine, commits));
+            var commitsSinceRelease = await context.GitHub().Client.Repository.Commit.GetAll(repositoryInfo.Owner,
+                repositoryInfo.RepositoryName, new CommitRequest
+                {
+                    Sha = "main",
+                    Since = latestRelease.CreatedAt.AddMinutes(-2),
+                });
 
-        return commits;
+            var commits = commitsSinceRelease
+                .Where(x => x.Author.Login.StartsWith("dependabot"))
+                .Select(x => x.Commit.Message.Split(Environment.NewLine))
+                .Select(x => x.FirstOrDefault())
+                .OfType<string>()
+                .ToList();
+
+            context.Logger.LogInformation("Commits: {Commits}", string.Join(Environment.NewLine, commits));
+
+            return commits;
+        }
+        catch (NotFoundException)
+        {
+            context.Logger.LogError("Release not found");
+        }
+
+        return new List<string> { };
     }
 }
