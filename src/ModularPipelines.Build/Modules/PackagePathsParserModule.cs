92c08529306a1715fb41d084ec14e533c1f5a7f7
ahaw <ahaw@o2.pl>
2025-01-11 23:01:22 +0100

    Fix failing module

--- src/ModularPipelines.Build/Modules/PackagePathsParserModule.cs
+++ src/ModularPipelines.Build/Modules/PackagePathsParserModule.cs
@@ -17,9 +17,10 @@ public class PackagePathsParserModule : Module<List<File>>
 		return packPackagesModuleResult.Value!
 			.Where(x => !string.IsNullOrWhiteSpace(x.StandardOutput))
 			.Select(x => x.StandardOutput)
+            .Where(x=>x.Contains("Successfully created package "))
             .Select(x => x.Split("Successfully created package '")?[1])
             .Where(x=> x!= null)
-            .Select(x => x.Split("'.")[0])
+			.Select(x => x.Split("'.")[0])
             .Select(x => new File(x))
             .ToList();
     }
