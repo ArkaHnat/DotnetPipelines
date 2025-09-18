8a659520031aa220f26afce2dd2c1f85d1d6519d
ArkaHnat <ahaw@o2.pl>
2025-09-18 06:35:56 +0200

    Fix fail of DependabotCommitsModule if no release

--- src/ModularPipelines.Build/Modules/DependabotCommitsModule.cs
+++ src/ModularPipelines.Build/Modules/DependabotCommitsModule.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using ModularPipelines.Attributes;
 using ModularPipelines.Context;
 using ModularPipelines.GitHub.Attributes;
 using ModularPipelines.GitHub.Extensions;
@@ -13,26 +12,35 @@ public class DependabotCommitsModule : Module<List<string>>
 {
     protected override async Task<List<string>?> ExecuteAsync(IPipelineContext context, CancellationToken cancellationToken)
     {
-		var repositoryInfo = context.GitHub().RepositoryInfo;
+        try
+        {
+            var repositoryInfo = context.GitHub().RepositoryInfo;
 
-		var latestRelease = await context.GitHub().Client.Repository.Release.GetLatest(repositoryInfo.Owner, repositoryInfo.RepositoryName);
+            var latestRelease = await context.GitHub().Client.Repository.Release.GetLatest(repositoryInfo.Owner, repositoryInfo.RepositoryName);
 
-		var commitsSinceRelease = await context.GitHub().Client.Repository.Commit.GetAll(repositoryInfo.Owner,
-			repositoryInfo.RepositoryName, new CommitRequest
-			{
-				Sha = "main",
-				Since = latestRelease.CreatedAt.AddMinutes(-2),
-			});
+            var commitsSinceRelease = await context.GitHub().Client.Repository.Commit.GetAll(repositoryInfo.Owner,
+                repositoryInfo.RepositoryName, new CommitRequest
+                {
+                    Sha = "main",
+                    Since = latestRelease.CreatedAt.AddMinutes(-2),
+                });
 
-		var commits = commitsSinceRelease
-			.Where(x => x.Author.Login.StartsWith("dependabot") || x.Author.Login.StartsWith("renovate-bot"))
-			.Select(x => x.Commit.Message.Split(Environment.NewLine))
-			.Select(x => x.FirstOrDefault())
-			.OfType<string>()
-			.ToList();
+            var commits = commitsSinceRelease
+                .Where(x => x.Author.Login.StartsWith("dependabot") || x.Author.Login.StartsWith("renovate-bot"))
+                .Select(x => x.Commit.Message.Split(Environment.NewLine))
+                .Select(x => x.FirstOrDefault())
+                .OfType<string>()
+                .ToList();
 
-		context.Logger.LogInformation("Commits: {Commits}", string.Join(Environment.NewLine, commits));
+            context.Logger.LogInformation("Commits: {Commits}", string.Join(Environment.NewLine, commits));
 
-		return commits;
-	}
+            return commits;
+        }
+        catch (Exception)
+        {
+            context.Logger.LogInformation("No release found. Cannot calculate commits since release.");
+        }
+
+        return ["Unknown"];
+    }
 }
