031bc671458dbb0efdbb6ade6a49ff6c127e1af4
ArkaHnat <ahaw@o2.pl>
2025-10-23 19:25:34 +0200

    chore(deps): Updated dotnet tool [dotnet-ef] from version [9.0.9] to version [9.0.10]

--- src/ModularPipelines.Build/GlobalSuppressions.cs
+++ src/ModularPipelines.Build/GlobalSuppressions.cs
@@ -3,6 +3,3 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1027:Use tabs correctly", Justification = "<Pending>", Scope = "member", Target = "~M:ModularPipelines.Build.Modules.GitCommitDotnetPackagesUpdates.ExecuteAsync(ModularPipelines.Context.IPipelineContext,System.Threading.CancellationToken)~System.Threading.Tasks.Task{ModularPipelines.Models.CommandResult}")]
